d3a124d99e85bdd6143f8d0684b28fb45f8ca04c
Seo-Jooyoung <tjwndud0412@naver.com>
2025-04-25 16:26:01 +0900

    [feat] 프로젝트 생성

---
+++ TVing/TVing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVing
+//
+//  Created by seozero on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
