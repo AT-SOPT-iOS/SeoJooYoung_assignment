a7cefbee49174c690da11b6580c1870a2cc78115
Seo-Jooyoung <tjwndud0412@naver.com>
2025-05-28 22:54:33 +0900

    [add] 에셋 및 폰트 추가

---
+++ TVing_SwiftUI/TVing_SwiftUI/Feature/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  TVing_SwiftUI
+//
+//  Created by seozero on 5/28/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ TVing_SwiftUI/TVing_SwiftUI/Feature/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  TVing_SwiftUI
+//
+//  Created by seozero on 5/28/25.
+//
+
+import Foundation
